15a584012d34a56f3d66302f2bba90f6d0a95f55
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-08-06 17:49:41 -0500

    script.js updated: function to start quiz created

--- js/script.js
+++ js/script.js
@@ -5,3 +5,21 @@ var startButton = document.getElementById("start");
 var submitButton = document.getElementById("submit");
 var initialsElement = document.getElementById("initials");
 var feedbackElement = document.getElementById("feedback");
+
+var time = questions.length * 15;
+var timerId;
+var currentQuestion = 0;
+
+function quizStart() {
+
+    var quizStartElement = document.getElementById("quiz-start");
+    quizStartElement.setAttribute("class", "hide");
+
+    questionsElement.removeAttribute("class");
+
+    timerId = setInterval(runTime, 1000);
+
+    timerElement.textContent = time;
+
+    showQuestion();
+}
