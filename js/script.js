92955e5f8f1670a4055172d0bf9f6554c01444e9
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-08-06 11:33:09 -0500

    DOM element variables created

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var timerElement = document.getElementById("time");
+var questionsElement = document.getElementById("questions");
+var answersElement = document.getElementById("answers");
+var startButton = document.getElementById("start");
+var submitButton = document.getElementById("submit");
+var initialsElement = document.getElementById("initials");
+var feedbackElement = document.getElementById("feedback");
