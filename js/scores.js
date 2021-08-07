9710c87c6a0bbdf072ce0a64e3345b33bdd7314f
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-08-06 20:40:00 -0500

    scripts.js updated & scores.js created

---
+++ js/scores.js
@@ -0,0 +1,23 @@
+function printScores() {
+
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+    highscores.sort(function(a, b) {
+        return b.score - a.score;
+    });
+
+    highscores.forEach(function(score) {
+        var liTag = document.createElement("li");
+        liTag.textContent = score.initials + "-" + score.score;
+
+        var olElement = document.getElementById("highscores");
+        olElement.appendChild(liTag);
+    });
+}
+
+    function clearScores() {
+        window.localStorage.removeItem("highscores");
+        window.location.reload();
+    }
+
+    document.getElementById("clear").onclick = clearScores;
--- js/script.js
+++ js/script.js
@@ -118,29 +118,5 @@ function saveScore() {
     }
 }
 
-function printScores() {
-
-    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
-
-    highscores.sort(function(a, b) {
-        return b.score - a.score;
-    });
-
-    highscores.forEach(function(score) {
-        var liTag = document.createElement("li");
-        liTag.textContent = score.initials + "-" + score.score;
-
-        var olElement = document.getElementById("highscores");
-        olElement.appendChild(liTag);
-    });
-}
-
-    function clearScores() {
-        window.localStorage.removeItem("highscores");
-        window.location.reload();
-    }
-
-    document.getElementById("clear").onclick = clearScores;
-
 startButton.onclick = quizStart;
 submitButton.onclick = saveScore;
