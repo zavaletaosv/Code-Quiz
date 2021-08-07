6abb57d0d1bb0a9a7987bc96f653248d51086c10
Osvaldo Zavaleta <osvzavaleta@gmail.com>
2021-08-06 20:35:12 -0500

    scripts.js updated: function to print scores created

--- js/script.js
+++ js/script.js
@@ -117,5 +117,30 @@ function saveScore() {
         window.location.href = "scores.html";
     }
 }
+
+function printScores() {
+
+    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+    highscores.sort(function(a, b) {
+        return b.score - a.score;
+    });
+
+    highscores.forEach(function(score) {
+        var liTag = document.createElement("li");
+        liTag.textContent = score.initials + "-" + score.score;
+
+        var olElement = document.getElementById("highscores");
+        olElement.appendChild(liTag);
+    });
+}
+
+    function clearScores() {
+        window.localStorage.removeItem("highscores");
+        window.location.reload();
+    }
+
+    document.getElementById("clear").onclick = clearScores;
+
 startButton.onclick = quizStart;
 submitButton.onclick = saveScore;
